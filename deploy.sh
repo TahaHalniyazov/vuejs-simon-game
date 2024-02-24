0123c969c74338646c9cabdd9cbdb8d717cc5164
unknown <halniyazovt98@gmail.com>
2024-02-24 17:24:12 +0300

    First-commit

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,10 @@ git init
 git add -A
 git commit -m 'Deploy to GitHub Pages'
 
-git push -f git@github.com:TahaHalniyazov/simon-game-vuejs.git master 
+
+git remote add origin git@github.com:TahaHalniyazov/simon-game-vuejs.git
+
+
+git push -f origin HEAD:main
 
 cd -
